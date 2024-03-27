cfde5aa5d06acc03667a247b46840de012ca10c9
MicheliniDev <michelinidev@gmail.com>
2024-03-27 10:40:43 -0300

    Adicionando arquivos

---
+++ ProjetoNadador/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ProjetoNadador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtD
+            // 
+            this.txtD.Location = new System.Drawing.Point(399, 173);
+            this.txtD.Name = "txtD";
+            this.txtD.Size = new System.Drawing.Size(122, 20);
+            this.txtD.TabIndex = 0;
+            this.txtD.TextChanged += new System.EventHandler(this.txtD_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Qual a distância percorrida?";
+            // 
+            // txtR
+            // 
+            this.txtR.Enabled = false;
+            this.txtR.Location = new System.Drawing.Point(331, 349);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(122, 20);
+            this.txtR.TabIndex = 2;
+            this.txtR.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtR;
+    }
+}
+
